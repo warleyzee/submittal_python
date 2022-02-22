33e9dafb4f8b056c8dbb8892e84ce6ded90f3cfd
unknown <warleyzee@hotmail.com>
2022-02-22 16:40:31 +0000

    [UPDATE] - First Update

---
+++ information.py
@@ -0,0 +1,62 @@
+from datetime import date
+
+
+class Information(): 
+    def date_current(self,):
+        
+        data = date.today()
+        month = data.month
+        year = data.year
+
+        if month == 1:
+            month = 'Jan'
+        elif month == 2:
+            month = 'Fev'
+        elif month == 3:
+            month = 'Mar'
+        elif month == 4:
+            month = 'Apr'
+        elif month == 5:
+            month = 'May'
+        elif month == 6:
+            month = 'Jun'
+        elif month == 7:
+            month = 'Jul'
+        elif month == 8:
+            month = 'Aug'
+        elif month == 9:
+            month = 'Sep'
+        elif month == 10:
+            month = 'Oct'
+        elif month == 11:
+            month = 'Nov'
+        elif month == 12:
+            month = 'Dec'
+
+        self.data_current = '{} {}'.format(month,year)
+        return self. data_current
+
+    def write_information(self,):
+        
+        data = Information()
+        data.date_current()
+        data_current = data.date_current()
+        self.information = []
+
+        submittal = str(input("Name Submittal: "))
+        description = str(input("Discription: "))
+        to_submittal = str(input("Submitall To: "))
+        no_submittal = str(input("Submitall No: "))
+        date_submittal = str(input("Date Submitall: "))
+        response = str(input("Response Required By: "))
+        info = str(input("Information: "))
+        
+
+        self.information = submittal, description, to_submittal, no_submittal, date_submittal, response,info, data_current
+
+        return(self.information)
+
+
+# test = Information()
+# test.write_information()
+# print(test.write_information())
---
+++ salve_file.py
@@ -0,0 +1,43 @@
+from docxtpl import DocxTemplate
+import pandas as pd
+import jinja2
+import re 
+from docx2pdf import convert
+from information import Information
+
+class Save_File():
+
+    def data_information(self,):
+        information = Information()
+        data = information.write_information()
+        
+        submittal = data[0]    
+        description = data[1]
+        to_submittal = data[2]
+        no_submittal = data[3]
+        date_submittal = data[4]
+        response = data[5]
+        info = data[6]
+        date_current = data[7]
+
+        doc = DocxTemplate("Submittal.docx")
+        context = { 'submittal': submittal,    
+                    'description': description,
+                    'to': to_submittal,
+                    'no': no_submittal,
+                    'date_sub': date_submittal,
+                    'response': response,
+                    'info': info, 
+                    'date_current': date_current}
+        doc.render(context)
+        doc.save("Submittal #{} - {}.docx".format(no_submittal, submittal))
+        # doc.save("teste.docx")
+
+        convert(r"C:\Users\Warley Souza\Music\Submittal\Submittal #{} - {}.docx".format(no_submittal, submittal),
+                r"C:\Users\Warley Souza\Music\Submittal\Submittal #{} - {}.pdf".format(no_submittal, submittal))  
+            
+
+test = Save_File()
+print(test.data_information())
+
+
