a03431ff0f479e991d40da2f1613603194eefbe6
unknown <warleyzee@hotmail.com>
2022-02-25 15:18:58 +0000

    Send

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from salve_file import Save_File
 from move_file import Move_file
+from send_email import Send_Email
 
-save = Save_File()
-get_save = save.data_information()
 
-move = Save_File()
-move.move_file()
+
+# save = Save_File()
+# get_save = save.data_information()
+
+# move = Save_File()
+# move.move_file()
+
+
+send = Send_Email()
+send.send_email()
